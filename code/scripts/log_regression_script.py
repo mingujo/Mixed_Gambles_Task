bc75e4113069f422e98977e7c69b4af5f9b70dfe
Min Gu Jo <mingu08@berkeley.edu>
2015-12-12 12:41:57 -0800

    changes made on the log reg script. commit before running

--- code/scripts/log_regression_script.py
+++ code/scripts/log_regression_script.py
@@ -1,7 +1,19 @@
-""" Logistic Regression on Begavioral data """
+"""
+Purpose:
+-----------------------------------------------------------------------------------
+We try to capture the significance of gain and loss amount condition for each subjects.
+We fit the logistic regression line based on their responses on the experiment. The slope 
+of the fitted line illustrates the subject's sensitivity on either gain or loss amount. 
 
+This script outputs plots for each subject and combine them into one image of subplots. 
+-----------------------------------------------------------------------------------
+"""
+
+from __future__ import absolute_import, division, print_function
 import sys, os
-sys.path.append(".././utils")
+#TODO : later change this
+sys.path.append(os.path.join(os.path.dirname(__file__), "../utils"))
+import numpy as np
 import numpy as np
 import matplotlib.pyplot as plt
 import nibabel as nib
@@ -11,23 +23,22 @@ from organize_behavior_data import *
 
 
 # Create the necessary directories if they do not exist
-dirs = ['../../txt_output', '../../txt_output/conv_normal',\
-        '../../fig','../../fig/log_reg_behav']
+dirs = ['../../fig','../../fig/log_reg_behav']
 for d in dirs:
     if not os.path.exists(d):
             os.makedirs(d)
 
 # Locate the different paths
 #TODO: the current location for this file project-epsilon/code/scripts
-project_path = '../../'
+project_path = '../../../'
 # TODO: change it to relevant path
 data_path = project_path+'data/ds005/'
-
-#change here to get your subject !
 subject_list = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16']
 
 images_paths = ['ds005_sub' + s.zfill(3) +'_log_reg_behav' for s in subject_list]
 
+fig = plt.figure()
+fig.suptitle("Fitted Logistic Regression Line (1(gamble) 0(not gamble) with gain and loss values", fontsize=12)
 for i,subject in enumerate(subject_list):
 	behav_df = load_in_dataframe(subject)
 	add_lambda = add_gainlossratio(behav_df)
@@ -38,28 +49,25 @@ for i,subject in enumerate(subject_list):
 	# calculate intercept and slope
 	intercept = -logit_pars['Intercept'] / logit_pars['gain']
 	slope = -logit_pars['loss'] / logit_pars['gain']
-	fig = plt.figure(figsize = (10, 8))   
+		#fig = plt.figure(figsize = (10, 8))   
+	ax = plt.subplot("44%s"%(str(i+1)))
+	ax.set_title("Subject_%s_run001"%(str(i+1)))
 
 	# plot gain and loss for respcat = 1(decides to gamble)
-	plt.plot(behav_df[behav_df['respcat'] == 1].values[:,2], behav_df[behav_df['respcat'] == 1].values[:,1], '.', label = "Gamble", mfc = 'None', mec='red')
+	l1 = ax.plot(behav_df[behav_df['respcat'] == 1].values[:,2], behav_df[behav_df['respcat'] == 1].values[:,1], '.', label = "Gamble", mfc = 'None', mec='red')
 
 	# plot gain and loss for respcat = 0(decides to not gamble)
-	plt.plot(behav_df[behav_df['respcat'] == 0].values[:,2], behav_df[behav_df['respcat'] == 0].values[:,1], '.', label = "Not gamble", mfc = 'None', mec='blue')
+	l2 = ax.plot(behav_df[behav_df['respcat'] == 0].values[:,2], behav_df[behav_df['respcat'] == 0].values[:,1], '.', label = "Not gamble", mfc = 'None', mec='blue')
 
 	# draw regression line
 	plt.plot(behav_df['loss'], intercept + slope * behav_df['loss'],'-', color = 'green') 
 
-	plt.xlabel('Loss ($)')
-	plt.ylabel('Gain ($)')
-	plt.legend(loc='bottom right')
+	ax.xlabel('Loss ($)')
+	ax.ylabel('Gain ($)')
 	plt.axis([2, 23, 8, 41])
 	plt.title("Subject_%s_Fitted Logistic Regression Line (1(gamble) 0(not gamble) with gain and loss values)\n"%(images_paths[i]))
-	plt.savefig(dirs[3]+'/'+images_paths[i])
-	plt.clf()
+	
+fig.legend((l1,l2), ('Gamble','Not Gamble', loc = 'lower right')
+plt.savefig(dirs[3]+'/log_regression_behav_subplots')
 
-
-# behav_df = load_in_dataframe(3)
-# a=add_gainlossratio(behav_df)
-# b=organize_columns(a)
-# log_regression(b)
 
